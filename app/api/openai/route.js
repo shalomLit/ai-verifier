143aedb4a3e822f39c4e31c044f312c921470a8e
shalomLit <shdover0@gmail.com>
2023-08-16 14:45:55 +0300

    improve ui

--- app/api/openai/route.js
+++ app/api/openai/route.js
@@ -13,16 +13,12 @@ export async function POST(req) {
   const { messages } = await req.json()
   console.log(messages, "messages");
   if (messages !== undefined) {
+    const content = `If the following statement is a fact, meaning the result is either true or false, you should respond with one of the following: "Yes, you are right." or "No, you are not right this time." If it is not a fact and it's a question, please respond with: "Sorry, but you should ask a question that can be answered with either true or false". Try to respond to the following sentence following the rules we defined: ${messages}`;
     const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: [{role: "user", content: `${messages}`}],
+      messages: [{role: "user", content: `${content}`}],
       max_tokens: 50,
     });
-      // const response = await openai.createCompletion({
-      //   model: "text-davinci-003",
-      //   prompt: `${messages}`,
-      //   max_tokens: 10,
-      // });
     console.log(response, "response");
     const resText = response.data.choices[0].message.content;
     console.log(resText, "resText");
--- components/prompt.jsx
+++ components/prompt.jsx
@@ -1,44 +1,69 @@
 'use client'
 import axios from "axios";
-import { useState, useEffect } from "react"
+import { useState } from "react";
+import { FaSmile, FaSadTear } from "react-icons/fa";
+import { RIGHT_RESPONSE, WRONG_RESPONSE, INVALID_RESPONSE } from "../constant/response";
+
 const Prompt = () => {
+  const [text, setText] = useState("");
+  const [response, setResponse] = useState("");
 
-    const [text, setText] = useState('');
-    const [response, setResponse] = useState('');
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
 
-    const handleTextChange = (e) => {
-        setText(e.target.value);
-      };
-
-    const handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-        console.log("111");
-        const response = await axios.post("/api/openai", { messages: text });
-        setResponse(response.data);
-        console.log("222");
-        console.log(response.data);
-        console.log(`The text is ${isTrue ? "true" : "false"}`);
-      } catch (error) {
-        console.log("333");
-        console.error(error);
-      }
-    };
-    return(
-        <section className="prompt">
-            <form className="relative w-full flex-center flex-col" onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="What is your opinion?"
-                    value={text}
-                    onChange={handleTextChange}
-                    className="search_input peer"
-                />
-                <button type="submit" className="outline_btn mt-6 justify-end">Submit</button>
-            </form>
-            {response && <p>{response}</p>}
-        </section>
-    )
-}
+      const response = await axios.post("/api/openai", { messages: text });
+      setResponse(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <section className="prompt">
+      <form
+        className="relative w-full flex-center flex-col"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          placeholder="What is your opinion?"
+          value={text}
+          onChange={handleTextChange}
+          className="search_input peer"
+        />
+        <button type="submit" className={`mt-6 bg-amber-500 hover:bg-amber-600 text-white font-bold py-2 px-4 rounded-full ${!text ? 'opacity-50 cursor-not-allowed' : ''}`}>
+          Button
+        </button>
+      </form>
+      {response && (
+        <div className="mt-4">
+          {response === RIGHT_RESPONSE && (
+            <>
+              <p className="text-green-500">
+                {response}
+                <FaSmile className="inline-block ml-2" size={24} />
+              </p>
+            </>
+          )}
+          {response === WRONG_RESPONSE && (
+            <>
+              <p className="text-red-500">
+                {response}
+                <FaSadTear className="inline-block ml-2" size={24} />
+              </p>
+            </>
+          )}
+          {response === INVALID_RESPONSE && (
+            <p className="text-yellow-500">{response}</p>
+          )}
+        </div>
+      )}
+    </section>
+  );
+};
 
 export default Prompt;
